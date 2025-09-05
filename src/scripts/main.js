9244b639f2d6b32d1e621cac79bd2924d0cf5cad
Sergey Zubinskiy <sergey.zubinskiy@skynix.co>
2025-09-05 15:18:42 +0300

    implmenetation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+/**
+ * Fixes form on pages as per task description
+ *
+ * @param {string} formSelector
+ */
+function fixForm(formSelector) {
+  if (!formSelector || typeof formSelector !== 'string') {
+    throw new Error('Invalid form selector');
+  }
+
+  const forms = document.querySelectorAll(formSelector);
+  const inputSelector = 'input';
+
+  for (const form of forms) {
+    const inputs = form.querySelectorAll(inputSelector);
+
+    for (const input of inputs) {
+      input.insertAdjacentHTML(
+        'beforebegin',
+        '<label class="field-label" for="' +
+          input.id +
+          '">' +
+          input.name.toUpperCase() +
+          '</label>',
+      );
+
+      input.setAttribute(
+        'placeholder',
+        input.name.charAt(0).toUpperCase() + input.name.slice(1),
+      );
+    }
+  }
+}
+
+fixForm('form'); // use html tag as selector
